5a5c839f20099d08c050528305777f965047c107
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-12-25 13:02:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,12 @@ import AddLeave from './components/AddLeave';
 import LeaveList from './components/LeaveList';
 import AddPayroll from './components/AddPayroll';  // Import AddPayroll component
 import PayrollList from './components/PayrollList'; 
+import DepartmentList from './components/DepartmentList';
+import AddDepartment from './components/AddDepartment';
+import AttendanceList from './components/AttendanceList';
+import AddAttendance from './components/AddAttendance';
+import QualificationForm from './components/QualificationForm';  // Import the QualificationForm component
+import EmployeeDetailsForm from './components/EmployeeDetailsForm'; 
 
 function Home() {
     return (
@@ -21,6 +27,24 @@ function Home() {
                 </button>
                 <button>
                     <Link to="/view-employees">View Employee Details</Link>
+                </button>
+                <button>
+                    <Link to="/add-qualification">Add Qualification</Link> {/* Button for QualificationForm */}
+                </button>
+                <button>
+                    <Link to="/add-employee-details">Add Employee Details</Link> {/* Button for EmployeeDetailsForm */}
+                </button>
+                <button>
+                    <Link to="/add-department">Add Department</Link>
+                </button>
+                <button>
+                    <Link to="/view-departments">View Department Details</Link>
+                </button>
+                <button>
+                    <Link to="/add-attendance">Add Attendance</Link>
+                </button>
+                <button>
+                    <Link to="/view-attendance">View Attendance Records</Link>
                 </button>
                 <button>
                     <Link to="/add-salary">Add Salary Details</Link>
@@ -61,6 +85,12 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/add-employee" element={<AddEmployee />} />
                     <Route path="/view-employees" element={<EmployeeList />} />
+                    <Route path="/add-qualification" element={<QualificationForm />} /> {/* Add route for QualificationForm */}
+                    <Route path="/add-employee-details" element={<EmployeeDetailsForm />} /> {/* Add route for EmployeeDetailsForm */}
+                    <Route path="/add-department" element={<AddDepartment />} />
+                    <Route path="/view-departments" element={<DepartmentList />} />
+                    <Route path="/add-attendance" element={<AddAttendance />} />
+                    <Route path="/view-attendance" element={<AttendanceList />} />
                     <Route path="/add-salary" element={<AddSalary />} />
                     <Route path="/view-salary/:eid" element={<Salary />} />
                     <Route path="/add-family" element={<FamilyDetailsForm />} />
